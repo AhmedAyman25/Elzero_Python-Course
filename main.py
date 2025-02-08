aec94ef12acc73048f2a6fa8f8ced2efccd8ef54
AhmedAyman25 <ahmed.ayman2516@gmail.com>
2025-02-08 23:14:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# height = [1.80, 1.75, 1.70, 1.65, 1.60]
+# weight = [80, 70, 65, 55, 50]
+
+
+# x = [1, 2, 3, 4, 5]
+# print(all(x))
+# print(any(x))
+# print(dir())
+# print(divmod(10,3))
+# first = [11.25, 18.0, 20.0]
+# second = [10.75, 9.50]
+# first.append(100)
+# first.remove(18.0)
+# print(first)
+# first.reverse()
+# print(first)
+################################################################
