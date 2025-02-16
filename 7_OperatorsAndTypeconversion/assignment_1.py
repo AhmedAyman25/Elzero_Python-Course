540cbeeae15fb57443cd50aec3f977cd1ae130ea
AhmedAyman25 <ahmed.ayman2516@gmail.com>
2025-02-17 00:08:28 +0200

    Operators and Type Conversion Solutions

---
+++ 7_OperatorsAndTypeconversion/assignment_1.py
@@ -0,0 +1,17 @@
+print(bool("Hi")) # True
+print(bool(100)) # True
+print(bool(" ")) # True
+print(bool(True)) # True
+print(bool([1,2,3])) # True
+
+print(bool("")) # False
+print(bool(0)) # False
+print(bool([])) # False
+print(bool(None)) # False
+print(bool(False)) # False
+print(bool({})) # False
+print(bool(())) # False
+
+
+
+
---
+++ 7_OperatorsAndTypeconversion/assignment_4.py
@@ -0,0 +1,11 @@
+num_one = 10
+num_two = 20
+
+result = num_one + num_two
+print(result) # 30
+result **= 3
+print(result) # 27000
+result %=  26000
+print(result) # 1000
+print(result / 5) # 200.0
+print(type(str(result))) # <class 'str'>
